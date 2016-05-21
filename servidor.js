137bd1b3b349dd991b870bb8bf7405d383985712
dhasuda <davihasuda@hotmail.com>
2016-05-21 16:36:16 -0300

    introducao do banco de dados mongodb

--- servidor.js
+++ servidor.js
@@ -1,14 +1,51 @@
+// Variaveis do servidor
 var http = require('http');
 var fs = require('fs');
 var app = require('express');
 
+// Variaveis do banco de dados
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+
+/*Banco de dados - Mongodb*/
+db.on('error', console.error); // Avisa que houve erro na conexao
+db.once('open', function(){
+	console.log('Conectado ao MongoDB');
+});
+
+
+// aluno
+var alunoSchema = new mongoose.Schema({
+	nome: String,
+	email: String,
+	senha: String,
+	curso: String,
+	serie: String
+});
+
+var Aluno = mongoose.model('Aluno', alunoSchema);
+
+// mentor
+var mentorSchema = new mongoose.Schema({
+	nome: String,
+	email: String,
+	senha: String,
+	curso: String,
+	ano: String
+});
+
+var Mentor = mongoose.model('Mentor', mentorSchema);
+
+mongoose.connect('mongodb://localhost/test');
+
+//Servidor
 function send404Error(res) {
 	res.writeHead(404, {"Content-Type": "text/plain"});
 	res.write("Error 404: Page not found");
 	res.end();
 }
 
-functino onRequest(req, res) {
+function onRequest(req, res) {
 
 }
 
