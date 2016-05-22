dd27a0d38b7e203b8bafa1ebc68d6562780c572e
dhasuda <davihasuda@hotmail.com>
2016-05-22 00:44:37 -0300

    funcao para registrar aluno

--- servidor.js
+++ servidor.js
@@ -20,44 +20,81 @@ db.once('open', function() {
 // Banco de dados --------------------------------------------------
 
 var alunoSchema = new mongoose.Schema({
-	nome: String
+	usuario: String,
+	email: String,
+	password: String,
+	first_name: String,
+	last_name: String,
+	decidido: String,
+	vestibulares: Array, // Nao esquecer de dar split()
+	dificuldades: Array, // Cada elemento do array corresponde a uma materia
+	// 1: tem dificuldade; 0: nao tem dificuldade
+	// Ordem das materias: mat, fis, quim, bio, hist, geo, port, red, filo, socio
+	facilidades: Array,
+	serie: String 
 });
 var Aluno = mongoose.model('Aluno', alunoSchema);
 
-var novoAluno = new Aluno({
-	nome: 'davi'
-});
+var mentorSchema = new mongoose.Schema ({
 
-novoAluno.save(function(err, novoAluno) {
-	if (err) return console.error(err);
-});
-
-// Aluno.remove(function (err) {
-//   if (err) return handleError(err);
-  // removed!
-//});
-
-Aluno.find(function(err, alunos) {
-	if (err) return console.error(err);
-	console.log(alunos);
-})
-
-Aluno.count({}, function( err, count){
-    console.log( "Number of users:", count );
-})
-
-// Mentor
-var mentorSchema = new mongoose.Schema({
-	nome: String
 });
 var Mentor = mongoose.model('Mentor', mentorSchema);
 
-
-
 // Servidor --------------------------------------------------------
-function submitAluno(req, res) {
-	console.log(req.body.first_name);
-	res.send(req.body.password);
+function onToInt(var str) {
+	if (str == 'on') return 1;
+	return 0;
+}
+
+function submitAl(req, res) {
+	if (req.body.password == req.body.password_confirmation){
+		var dif = [];
+		var fac = [];
+
+		dif.push(onToInt(req.body.dificuldade_mat));
+		dif.push(onToInt(req.body.dificuldade_fis));
+		dif.push(onToInt(req.body.dificuldade_quim));
+		dif.push(onToInt(req.body.dificuldade_bio));
+		dif.push(onToInt(req.body.dificuldade_hist));
+		dif.push(onToInt(req.body.dificuldade_geo));
+		dif.push(onToInt(req.body.dificuldade_port));
+		dif.push(onToInt(req.body.dificuldade_red));
+		dif.push(onToInt(req.body.dificuldade_filo));
+		dif.push(onToInt(req.body.dificuldade_socio));
+		fac.push(onToInt(req.body.facilidade_mat));
+		fac.push(onToInt(req.body.facilidade_fis));
+		fac.push(onToInt(req.body.facilidade_quim));
+		fac.push(onToInt(req.body.facilidade_bio));
+		fac.push(onToInt(req.body.facilidade_hist));
+		fac.push(onToInt(req.body.facilidade_geo));
+		fac.push(onToInt(req.body.facilidade_port));
+		fac.push(onToInt(req.body.facilidade_red));
+		fac.push(onToInt(req.body.facilidade_filo));
+		fac.push(onToInt(req.body.facilidade_socio));
+
+		var novoAluno = new Aluno({
+			usuario: req.body.usuario,
+			email: req.body.email,
+			password: req.body.password,
+			first_name: req.body.first_name,
+			last_name: req.body.last_name,
+			decidido: req.body.decidido,
+			vestibulares: req.body.vestiulares.split("/");
+			dificuldades: dif,
+			facilidades: fac,
+			serie: req.body.serie
+		});
+
+		novoAluno.save(function(err, novoAluno) {
+			if (err) return console.error(err);
+		});
+
+		res.writeHed(200, {"Cotent-Type": "text/html"});
+		fs.createReadStream("./sucesso.html").pipe(res);
+	}
+	else {
+		// Se a senha nao for confirmada corretamente
+	}
 	//res.writeHead(200, {"Cotent-Type": "text/plain"});
 	// res.write('Deu certo');
 	// res.end();
@@ -66,7 +103,26 @@ function submitAluno(req, res) {
 	//fs.createReadStream("./index.html").pipe(res);
 }
 
-app.post('/aluno', submitAluno);
+function submitMent(req, res) {
+	if (req.body.password == req.body.password_confirmation){
+		var novoMentor = new Mentor({
+			
+		});
+
+		novoMentor.save(function(err, novoMentor) {
+			if (err) return console.error(err);
+		});
+
+		res.writeHed(200, {"Cotent-Type": "text/html"});
+		fs.createReadStream("./sucesso.html").pipe(res);
+	}
+	else {
+		// Se a senha nao for confirmada corretamente
+	}
+}
+
+app.post('/submitAluno', submitAl);
+app.post('/submitMentor', submitMent);
 
 app.get('/', function(req, res){
 	res.writeHead(200, {"Cotent-Type": "text/html"});
